3a8e1061e2a2f5d03ec073ef0ee9e2c44b0de77b
Kelly Johnson <kjohnson@ngmoco.com>
2013-07-19 14:52:26 -0700

    Moved libs to submodule for size limit, added external debug capability to v8

--- src/platform-posix.cc
+++ src/platform-posix.cc
@@ -403,7 +403,7 @@ bool POSIXSocket::Bind(const int port) {
   sockaddr_in addr;
   memset(&addr, 0, sizeof(addr));
   addr.sin_family = AF_INET;
-  addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
   addr.sin_port = htons(port);
   int status = bind(socket_,
                     BitCast<struct sockaddr *>(&addr),
